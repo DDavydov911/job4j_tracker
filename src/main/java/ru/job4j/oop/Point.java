355b6da8c36b1cd6ba7466c84a41566d6cb53d96
Denis Davydov <dvd9@yandex.ru>
2021-05-05 20:47:04 +0300

    2.2. Рефакторинг - Площадь треугольника. [#455967]

---
+++ src/main/java/ru/job4j/oop/Point.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+    private double ab;
+    private double ac;
+    private double bc;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+        this.ab = ap.distance(bp);
+        this.ac = ap.distance(cp);
+        this.bc = bp.distance(cp);
+    }
+
+    public double period(double a, double b, double c) {
+        return (ab + ac + bc) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ab + bc > ac && bc + ac > ab;
+    }
+
+    public double area() {
+        double rsl = -1;
+//        double ab = first.distance(second);
+//        double ac = first.distance(third);
+//        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+}
