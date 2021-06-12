37995092db2fa1906b78246398b0503311056e03
Denis Davydov <dvd9@yandex.ru>
2021-06-12 17:50:19 +0300

    2. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/Main.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tracker st = SingleTracker.getInstance();
+        st.add(new Item("item1"));
+        System.out.println(st.findById(1));
+        Tracker st2 = SingleTracker.getInstance();
+        System.out.println(st2.findById(1));
+    }
+}
