3a2c48e9c5adbd8306f157f709ca1de1fc180b5b
Denis Davydov <dvd9@yandex.ru>
2021-06-14 22:25:17 +0300

    2. Singleton [#94606 #167024] fixed

--- src/main/java/ru/job4j/tracker/Main.java
+++ src/main/java/ru/job4j/tracker/Main.java
@@ -3,10 +3,10 @@ package ru.job4j.tracker;
 public class Main {
 
     public static void main(String[] args) {
-        Tracker st = SingleTracker.getInstance();
+        SingleTracker st = SingleTracker.getInstance();
         st.add(new Item("item1"));
         System.out.println(st.findById(1));
-        Tracker st2 = SingleTracker.getInstance();
+        SingleTracker st2 = SingleTracker.getInstance();
         System.out.println(st2.findById(1));
         st2.replace(1, new Item("item444"));
         System.out.println(st.findById(1));
--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,15 +1,40 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
-    private static Tracker instance = null;
+    private static Tracker tracker = new Tracker();
+    private  static SingleTracker instance = null;
 
     private SingleTracker() {
     }
 
-    public static Tracker getInstance() {
+    public static SingleTracker getInstance() {
         if (instance == null) {
-            instance = new Tracker();
+            instance = new SingleTracker();
         }
         return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public Item[] finedAll() {
+        return tracker.findAll();
     }
 }
