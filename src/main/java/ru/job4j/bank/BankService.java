77d3a111e9ca923f7b6f318ed1eff1eaf6de98c9
Denis Davydov <dvd9@yandex.ru>
2021-07-21 20:41:46 +0300

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,45 +1,56 @@
-//package ru.job4j.bank;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//public class BankService {
-//    private Map<User, List<Account>> users = new HashMap<>();
-//
-//    public void addUser(User user) {
-//        if (!users.containsKey(user)) {
-//            users.put(user, new ArrayList<>());
-//        }
-//    }
-//
-//    public void addAccount(String passport, Account account) {
-//        for (User user : users.keySet()) {
-//                List<Account> list = new ArrayList<>();
-//                list.add(account);
-//                users.put(user, list);
-//            }
-//        }
-//
-//    }
-//
-//    public User findByPassport(String passport) {
-//        for (User user : users.keySet()) {
-//            if (passport.equals(user.getPassport())) {
-//                return user;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    public Account findByRequisite(String passport, String requisite) {
-//        return null;
-//    }
-//
-//    public boolean transferMoney(String srcPassport, String srcRequisite,
-//                                 String destPassport, String destRequisite, double amount) {
-//        boolean rsl = false;
-//        return rsl;
-//    }
-//}
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        List<Account> list = users.get(user);
+        list.add(account);
+        users.put(user, list);
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        if (findByPassport(passport) != null) {
+            for (Account account : users.get(findByPassport(passport))) {
+                if (requisite.equals(account.getRequisite())) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (amount <= srcAccount.getBalance()) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.bank;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class BankServiceTest {
+
+    @Test
+    public void addUser() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        assertThat(bank.findByPassport("3434"), is(user));
+    }
+
+    @Test
+    public void whenEnterInvalidPassport() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 100D));
+        assertNull(bank.findByRequisite("34", "5546"));
+    }
+
+
+    @Test
+    public void addAccount() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        Account account = new Account("34dcl", 100D);
+        bank.addAccount(user.getPassport(), account);
+        assertThat(bank.findByRequisite("3434", "34dcl").getBalance(), is(100D));
+    }
+
+    @Test
+    public void transferMoney() {
+        User srcUser = new User("1234", "Ivan Ivanov");
+        Account srcAccount = new Account("0001", 7000D);
+        User destUser = new User("2222", "San Sanych");
+        Account destAccount = new Account("0002", 100D);
+        BankService bank = new BankService();
+        bank.addUser(srcUser);
+        bank.addAccount(srcUser.getPassport(), srcAccount);
+        bank.addUser(destUser);
+        bank.addAccount(destUser.getPassport(), destAccount);
+        bank.transferMoney(srcUser.getPassport(), srcAccount.getRequisite(),
+                destUser.getPassport(), destAccount.getRequisite(), 1000);
+        assertThat(destAccount.getBalance(), is(1100D));
+        assertThat(srcAccount.getBalance(), is(6000D));
+    }
+}
