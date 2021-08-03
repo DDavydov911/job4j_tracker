7e51fcf2ee2d821e807253901e118b41f820e004
Denis Davydov <dvd9@yandex.ru>
2021-08-03 11:22:58 +0300

    4. Сортировка номера [#221284 #185056] fixed

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,26 +6,7 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        String nums1 = "";
-        String nums2 = "";
-        for (int i = 0; i < o1.length(); i++) {
-            char n = o1.charAt(i);
-            if (n == '.') {
-                break;
-            }
-            if (Character.isDigit(n)) {
-                nums1 += n;
-            }
-        }
-        for (int i = 0; i < o2.length(); i++) {
-             char n = o2.charAt(i);
-            if (n == '.') {
-                break;
-            }
-             if (Character.isDigit(n)) {
-                 nums2 += n;
-             }
-        }
-        return Integer.compare(Integer.parseInt(nums1), Integer.parseInt(nums2));
+        return Integer.compare(Integer.parseInt(o1.split("\\.")[0]),
+                Integer.parseInt(o2.split("\\.")[0]));
     }
 }
