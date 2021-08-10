01182a759ffe998b5350a506886d60ed8f2622c6
Denis Davydov <dvd9@yandex.ru>
2021-08-10 16:11:03 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Profile {
     private Address address;
 
@@ -9,5 +11,29 @@ public class Profile {
 
     public Address getAddress() {
         return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{"
+                + "address=" + address
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Profile profile = (Profile) o;
+        return Objects.equals(address, profile.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -13,11 +13,19 @@ public class ProfilesTest {
     @Test
     public void collect() {
         Address adr1 = new Address("Moscow", "Pushkina", 14, 5);
-        Address adr2 = new Address("Moscow", "Novii Arbat", 27, 8);
-        List<Address> expected = List.of(adr1, adr2);
+        Address adr2 = new Address("Briansk", "Lenina", 9, 16);
+        Address adr3 = new Address("Moscow", "Novii Arbat", 27, 8);
+        Address adr4 = new Address("Moscow", "Novii Arbat", 27, 8);
+        Address adr5 = new Address("Moscow", "Novii Arbat", 27, 8);
+        Address adr6 = new Address("Vologda", "Pushkina", 14, 5);
+        List<Address> expected = List.of(adr2, adr1, adr3, adr6);
         List<Profile> profileList = Arrays.asList(
                 new Profile(adr1),
-                new Profile(adr2)
+                new Profile(adr2),
+                new Profile(adr3),
+                new Profile(adr4),
+                new Profile(adr5),
+                new Profile(adr6)
         );
         Profiles pfs = new Profiles();
         List<Address> result = pfs.collect(profileList);
