d2b282bb6fc7b3c963b368e198220b27d66d724f
Denis Davydov <dvd9@yandex.ru>
2021-08-11 10:07:38 +0300

    3. Уникальность элементов и сортировка. [#110225 #188166] ашчув

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,9 +8,9 @@ import java.util.stream.Collectors;
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .distinct()
                 .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
