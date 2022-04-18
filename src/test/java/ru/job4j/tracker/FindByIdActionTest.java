7b30122a693b7a9a33a78d76f5df065f8a612017
Denis Davydov <dvd9@yandex.ru>
2022-04-18 18:04:10 +0300

    1. Что такое Mock-объект. [#504866 #288480] fixed actions

--- src/test/java/ru/job4j/tracker/FindByIdActionTest.java
+++ src/test/java/ru/job4j/tracker/FindByIdActionTest.java
@@ -20,12 +20,23 @@ public class FindByIdActionTest {
         when(input.askInt(any(String.class))).thenReturn(1);
         FindByIdAction findByIdAction = new FindByIdAction(output);
         boolean result = findByIdAction.execute(input, tracker);
+        assertTrue(result);
+        assertThat(tracker.findById(1).getName(), is("My item"));
+    }
+
+    @Test
+    public void whenNotFindBiIdAction() {
+        Output output = new StubOutput();
+        MemTracker tracker = new MemTracker();
+        Item item = new Item("My item");
+        tracker.add(item);
+        Input input = mock(Input.class);
+        when(input.askInt(any(String.class))).thenReturn(2);
+        FindByIdAction findByIdAction = new FindByIdAction(output);
+        boolean result = findByIdAction.execute(input, tracker);
         String ls = System.lineSeparator();
         assertTrue(result);
-
-        /*
-        assertThat(output.toString().substring(0, 45), is("=== Find item by id ===" + ls
-                + "id: 1, name: My item"));
-         */
+        assertThat(output.toString(), is("=== Find item by id ===" + ls
+                + "Заявка с введенным id: 2 не найдена." + ls));
     }
 }
--- src/test/java/ru/job4j/tracker/FindByNameActionTest.java
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -24,11 +24,24 @@ public class FindByNameActionTest {
         assertTrue(result);
         assertThat(tracker.findByName("My item").get(0).getName(),
                 is("My item"));
-        /*
-        assertThat(output.toString().substring(0, 49),
-                is("=== Find items by name ====" + ls
-                + "id: 1, name: My item"));
-         */
+    }
+
+    @Test
+    public void whenNotFindByName() {
+        Output output = new StubOutput();
+        MemTracker tracker = new MemTracker();
+        Item item = new Item("My item");
+        tracker.add(item);
+        Input input = mock(Input.class);
+        when(input.askStr(any(String.class))).thenReturn("Not My item");
+        FindByNameAction findByNameAction = new FindByNameAction(output);
+        boolean result = findByNameAction.execute(input, tracker);
+        String ls = System.lineSeparator();
+        assertTrue(result);
+        assertThat(tracker.findByName("My item").get(0).getName(),
+                is("My item"));
+        assertThat(output.toString(), is("=== Find items by name ====" + ls
+                + "Заявки с именем: Not My item не найдены." + ls));
     }
 
 }
